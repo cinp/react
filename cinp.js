5989570bb69391ef26387796be918a4011908b6e
Peter Howe <pnhowe@gmail.com>
2019-05-29 11:43:03 -0600

    fixed logic error

--- cinp.js
+++ cinp.js
@@ -279,7 +279,7 @@ class CInP
       args.Position = position;
     }
 
-    if( count !== undefined || count !== '' )
+    if( count !== undefined && count !== '' )
     {
       args.Count = count;
     }
